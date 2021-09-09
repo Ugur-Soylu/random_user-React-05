7661d8c724224fbc08515aee70c2767ab706f289
ugur <muptedikaligraf@gmail.com>
2021-09-09 04:15:00 +0300

    random_user_app is created and published

---
+++ src/components/Card/Card.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from "react";
+import Email from "../svg/Email";
+import Location from "../svg/Location";
+import Phone from "../svg/Phone";
+import "./Card.css"
+import axios from "axios";
+
+
+
+const Card = () => {
+  const [user, setUser] = useState("")
+
+
+const getRandomUser = ()=>{
+  axios.get("https://randomuser.me/api/").then((response)=>{
+    setUser(response.data.results[0])
+    console.log(response.data.results[0])
+  })
+}
+
+
+useEffect(()=>{
+  getRandomUser()
+},[])
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="image-title-box">
+          <img src={user.picture?.large} alt=""/>
+          <p className="info">{user.name?.title} {user.name?.first} {user.name?.last}</p>
+        </div>
+        <div className="user-email">
+          <Email className="img"/>
+          <p className="info">{user.email}</p>
+        </div>
+        <div className="user-phone">
+          <Phone className="img"/>
+          <p className="info">{user.phone}</p>
+        </div>
+        <div className="user-location">
+          <Location className="img"/>
+          <p className="info">{user.location?.city} / {user.location?.country}</p>
+        </div>        
+        <div className="user-register">
+          <p className="info">Age:{user.dob?.age}</p>
+          <p className="info">Register Date:{user.dob?.date.slice(0,10)}</p>
+        </div>
+      </div>
+      <div>
+      <button className="button" onClick={getRandomUser}>Random User</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
+
+
